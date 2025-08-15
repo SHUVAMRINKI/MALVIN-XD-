916438fad7578be408a14165fa8cb7425445398a
SHUVAMRINKI <debojitdas4051@gmail.com>
2025-08-15 06:11:59 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~U2UVXT6I#m2NW6Q6c4HQpx8vuvR4xyHKgbHSceuY_FZQdtCwZFSU",  // add your Session Id here
+  SESSION_ID: "malvin~lzshRZjA#iZgz14ltSsYtDn9A_ldsuLyrGcMJQgFmYU5xYM4T0ls",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
